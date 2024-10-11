514773a135710bc57cb77e4b45dbe49c873c202b
Lalz209 <lalzpaisa@gmail.com>
2024-10-10 21:50:20 -0600

    update 3

--- app/routes.py
+++ app/routes.py
@@ -1,9 +1,14 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, jsonify, Flask
 
 main = Blueprint('main', __name__)
 
-@main.route('/')
+@main.route('/', methods=["GET", "POST"]) 
 def index():
+    if request.method == "POST":
+        
+        return 
+
+    
     return render_template('index.html')
 
 @main.route('/login')
